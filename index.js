42b0b6f1131b0c591411cde27fd66568627c189e
Wassim CHEGHAM <github@wassimchegham.com>
2017-11-13 22:44:04 +0100

    chore: better error handling

--- index.js
+++ index.js
@@ -1,38 +1,45 @@
 class I18n {
-    configure(options = {}) {
-
-        if (!options.directory) {
-            throw new Error(' "directory" is not valid. Please provide a directory of your locales.');
-        }
-
-        this.directory = options.directory;
-        this.defaultLocale = options.defaultLocale || "en-us";
-
-        return this;
+  configure(options = {}) {
+    if (!options.directory) {
+      throw new Error(
+        ' "directory" is not valid. Please provide a directory of your locales.'
+      );
     }
-    use(app) {
-        let locale = app.getUserLocale();
 
-        if (locale) {
-          locale = locale.toLowerCase();
-        }
-        else {
-          locale = this.defaultLocale;
-        }
+    this.directory = options.directory;
+    this.defaultLocale = options.defaultLocale || "en-us";
 
-        app.__ = (key, context = {}) => {
-            const locales = require(`${this.directory}/${locale}`);
-            let translation = locales[key];
+    return this;
+  }
+  use(app) {
+    let locale = app.getUserLocale();
 
-            if (translation) {
-                for (let ctxKey in context) {
-                    translation = translation.replace('{' + ctxKey + '}', context[ctxKey]);
-                }
-            }
-
-            return translation;
-        }
+    if (locale) {
+      locale = locale.toLowerCase();
+    } else {
+      locale = this.defaultLocale;
     }
+
+    app.__ = (key, context = {}) => {
+      try {
+        const locales = require(`${this.directory}/${locale}`);
+        let translation = locales[key] || {};
+
+        if (translation) {
+          for (let ctxKey in context) {
+            translation = translation.replace(
+              "{" + ctxKey + "}",
+              context[ctxKey]
+            );
+          }
+        }
+      } catch (e) {
+        throw new Error(`[actions-on-google-i18n] ${e}`);
+      }
+
+      return translation;
+    };
+  }
 }
 
 module.exports = new I18n();
