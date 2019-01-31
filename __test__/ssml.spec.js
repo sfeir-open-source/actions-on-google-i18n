63a676e43b5ae3e8b4a5c177018e2619e60330a3
Wassim Chegham <github@wassim.app>
2019-01-31 20:41:49 +0000

    chore: add support for SSML entry

--- __test__/ssml.spec.js
+++ __test__/ssml.spec.js
@@ -4,16 +4,48 @@ const directory = `${__dirname}/src/locales`;
 
 const mockApp = new AppMock();
 
-fdescribe("SSML", () => {
-  describe("when getUserLocale is NULL", () => {
+describe("SSML", () => {
+  let conv;
 
-    it("support SSML/TEXT entries", () => {
-      i18n.configure({ directory, defaultExtension: "json" }).use(mockApp);
-      const conv = mockApp.newConv("ssml");
+  beforeEach( () => {
+    i18n.configure({ directory }).use(mockApp);
+    conv = mockApp.newConv("ssml");
+  });
+
+  describe("support complex entries", () => {
+
+    it("SSML/TEXT entries", () => {
       expect(typeof conv.__("key")).toBe("object");
-      expect(typeof conv.__("key").ssml).toBeTruthy();
-      expect(typeof conv.__("key").text).toBeTruthy();
     });
+
+    it("simple TEXT entry", () => {
+      expect(typeof conv.__("key").text).toBe("string");
+    });
+
+    it("simple SSML entry", () => {
+      expect(typeof conv.__("key").ssml).toBeTruthy();
+    });
+
+    it("valid SSML markup", () => {
+      expect(conv.__("key").ssml.startsWith("<speak>")).toBeTruthy();
+      expect(conv.__("key").ssml.endsWith("</speak>")).toBeTruthy();
+    });
+
+    it("invalid entry key", () => {
+      expect(() => conv.__("invalid_key")).toThrow("Error: only 'text' and 'ssml' values are allowed.");
+    });
+  });
+
+  describe("support context substitution", () => {
+
+    it("for TEXT entry", () => {
+      expect(conv.__("key_2", {var: 2}).text).toBe("value 2");
+    });
+
+    it("for SSML entry", () => {
+      expect(conv.__("key_2", {var: "four"}).ssml).toBe("<speak><s>This is sentence two and four.</s></speak>");
+    });
+
   });
 
 });
